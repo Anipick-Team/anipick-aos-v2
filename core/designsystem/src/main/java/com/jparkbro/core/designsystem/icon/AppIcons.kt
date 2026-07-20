d1bd6a579ff86b0d709544d011e9cc132a130683
jparkbro <jparkbro@gmail.com>
2026-07-20 22:40:36 +0900

    [Feat] Icon 추가

---
+++ core/designsystem/src/main/java/com/jparkbro/core/designsystem/icon/AppIcons.kt
@@ -0,0 +1,106 @@
+package com.jparkbro.core.designsystem.icon
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import com.jparkbro.core.designsystem.R
+
+val AlertCircle: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(id = R.drawable.ic_alert_circle)
+
+val ArrowLeft: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(id = R.drawable.ic_arrow_left)
+
+val ArrowRight: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(id = R.drawable.ic_arrow_right)
+
+val Check: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(id = R.drawable.ic_check)
+
+val ChevronDown: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(id = R.drawable.ic_chevron_down)
+
+val ChevronLeft: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(id = R.drawable.ic_chevron_left)
+
+val ChevronRight: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(id = R.drawable.ic_chevron_right)
+
+val ChevronsDown: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(id = R.drawable.ic_chevrons_down)
+
+val ChevronsLeft: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(id = R.drawable.ic_chevrons_left)
+
+val ChevronsRight: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(id = R.drawable.ic_chevrons_right)
+
+val ChevronsUp: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(id = R.drawable.ic_chevrons_up)
+
+val Close: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(id = R.drawable.ic_close)
+
+val Comment: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(id = R.drawable.ic_comment)
+
+val Edit: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(id = R.drawable.ic_edit)
+
+val HeartFilled: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(id = R.drawable.ic_heart_filled)
+
+val HeartOutlined: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(id = R.drawable.ic_heart_outlined)
+
+val Image: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(id = R.drawable.ic_image)
+
+val MoreHorizontal: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(id = R.drawable.ic_more_horizontal)
+
+val MoreVertical: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(id = R.drawable.ic_more_vertical)
+
+val Search: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(id = R.drawable.ic_search)
+
+val Share: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(id = R.drawable.ic_share)
+
+val StarFilled: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(id = R.drawable.ic_star_filled)
+
+val StarOutlined: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(id = R.drawable.ic_star_outlined)
+
+val VisibilityOff: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(id = R.drawable.ic_visibility_off)
+
+val VisibilityOn: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(id = R.drawable.ic_visibility_on)
